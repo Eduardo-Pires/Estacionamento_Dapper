3adc3f99bff36443334b75b16ebf9bf3a2b6f232
Eduardo Pires <94654833+Eduardo-Pires@users.noreply.github.com>
2024-07-23 22:29:58 -0300

    alteração no tratamento de erro e modificação do controller

--- Estacionamento_dapper/estacionamento/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace estacionamento.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
